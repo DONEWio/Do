023fb722193ffacc9acb00e51e5ffeb5ac77e179
denizkenan <kenandnz@outlook.com>
2025-02-07 06:31:07 +0100

    Update local runtime license header with donew modifications
    - Added extended license header with donew copyright notice
    - Included comments about enhanced local runtime capabilities
    - Preserved original Apache License 2.0 text

--- src/donew/new/runtimes/local.py
+++ src/donew/new/runtimes/local.py
@@ -5,7 +5,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You may obtain a copy of the License at:
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -14,6 +14,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# ---------------------------------------------------------------------------
+# Modifications made by donew, Inc.
+# Additional enhancements and modifications:
+# - Enhanced local runtime with external final_answer tools call support.
+# Extended capabilities added to support DO.New enhanced structured output support.
+#
+# Copyright 2024 donew, Inc. All rights reserved.
+# ---------------------------------------------------------------------------
 import ast
 import builtins
 import difflib
